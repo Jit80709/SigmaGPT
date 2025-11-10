f198d39382756dbaf59b161552f20ffe0ebfd848
Jit80709 <jit80709@gmail.com>
2025-11-10 14:57:47 +0530

    Fixed Render Node22 path-to-regexp bug

--- Backend/server.js
+++ Backend/server.js
@@ -105,4 +105,4 @@ app.get(/.*/,  (req, res) => {
 // Launches Express app on PORT (default: 8080)
 
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => console.log(` Server running on port ${PORT}`));
+app.listen(PORT,"0.0.0.0", () => console.log(` Server running on port ${PORT}`));
